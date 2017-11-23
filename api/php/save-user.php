1c1450a46ef1f7c07d6ac411fbe97d18a919c195
Patrick <brix.bjerg@gmail.com>
2017-11-23 09:47:40 +0100

    Save user is fully working

--- api/php/save-user.php
+++ api/php/save-user.php
@@ -1,21 +1,21 @@
 <?
 
+// The database connection. You might need to change this if you're not using mamp!
 $servername = "localhost";
 $username = "root";
 $password = "root";
+// Please use the same database name
 $dbname = "kea_masterclasses";
 
 
-// INSERT INTO `users` (`name`, `email`, `password`, `notification`, `admin`, `image`) VALUES ('test', 'test', '123', '1', '1', 'test')
-
-
-
+    // Save the image with a unique ID
     $sFileExtension = pathinfo($_FILES['fileUserImage']['name'], PATHINFO_EXTENSION);
 	$sFolder = '../../app/assets/img/';
 	$sFileName = 'userimage-'.uniqid().'.'.$sFileExtension;
 	$sSaveFileTo = $sFolder.$sFileName;
 	move_uploaded_file( $_FILES['fileUserImage']['tmp_name'], $sSaveFileTo);
 
+    // Get the data from the client
     $sUserName = $_POST['txtSaveUserName'];
     $sUserEmail = $_POST['txtSaveUserEmail'];
     $sUserPassword = $_POST['txtSaveUserPassword'];
@@ -27,23 +27,24 @@ $dbname = "kea_masterclasses";
                 // connect to the database
                 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
                 // create a query
-                $query = $conn->prepare("INSERT INTO `users` (`name`, `email`, `password`, `notification`, `admin`, `image`) VALUES (:name, :email, :password, :notification, ':admin', ':image')");
+                $query = $conn->prepare("INSERT INTO `users` (`name`, `email`, `password`, `notification`, `admin`, `image`) VALUES (:name, :email, :password, :notification, '0', :image)");
                 
+                // Bind param, this if for security
                 $query->bindParam( ':name' , $sUserName );
                 $query->bindParam(':email' , $sUserEmail);
                 $query->bindParam(':password', $sUserPassword);
                 $query->bindParam(':notification' , $bNotification);
                 // Admin goes to false or "0" by default
                 $query->bindParam(':image', $sFilePath);
-                //$query->bindParam(':image', $sFilePath);
                 
                 // run the query
                 
                 $bResult = $query->execute();
+
+                // If the query is successful the bResult will be true
                 $sjResponse = $bResult ? '{"status":"ok"}' : '{"status":"error"}' ;
-                // $lastId = $query->lastInsertId();
-                //$result = json_encode( $query->fetch(PDO::FETCH_ASSOC) );
         
+                // Echo back to the client
                 echo $sjResponse; 
         
         
