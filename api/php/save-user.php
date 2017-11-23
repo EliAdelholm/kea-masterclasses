121d68f80acd14e0897aeaacafa9d12d8dd7d614
Patrick <brix.bjerg@gmail.com>
2017-11-23 14:29:55 +0100

    Added a adatabase dump and made some changes to save-user.php to fit the new schema (NOT FULLY DONE)

--- api/php/save-user.php
+++ api/php/save-user.php
@@ -7,6 +7,9 @@ $password = "root";
 // Please use the same database name
 $dbname = "kea_masterclasses";
 
+// INSERT INTO `USERS` (`id`, `name`, `password`, `notification`, `image`) VALUES ('521', 'Patrick', 'test', '1', 'test');
+
+
 
     // Save the image with a unique ID
     $sFileExtension = pathinfo($_FILES['fileUserImage']['name'], PATHINFO_EXTENSION);
@@ -16,22 +19,23 @@ $dbname = "kea_masterclasses";
 	move_uploaded_file( $_FILES['fileUserImage']['tmp_name'], $sSaveFileTo);
 
     // Get the data from the client
+    $sUserId = uniqid();
     $sUserName = $_POST['txtSaveUserName'];
     $sUserEmail = $_POST['txtSaveUserEmail'];
     $sUserPassword = $_POST['txtSaveUserPassword'];
+    $sUserPhone = $_POST['txtSaveUserPhone'];
     $bNotification = $_POST['checkNotification'];
     $sFilePath = 'assets/img/'.$sFileName;
-
 
     try {
                 // connect to the database
                 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
                 // create a query
-                $query = $conn->prepare("INSERT INTO `users` (`name`, `email`, `password`, `notification`, `admin`, `image`) VALUES (:name, :email, :password, :notification, '0', :image)");
+                $query = $conn->prepare("INSERT INTO `USERS` (`id`, `name`, `password`, `notification`, `image`) VALUES (:id, :name, :password, :notification, :image)");
                 
                 // Bind param, this if for security
+                $query->bindParam( ':id' , $sUserId );
                 $query->bindParam( ':name' , $sUserName );
-                $query->bindParam(':email' , $sUserEmail);
                 $query->bindParam(':password', $sUserPassword);
                 $query->bindParam(':notification' , $bNotification);
                 // Admin goes to false or "0" by default
@@ -39,19 +43,22 @@ $dbname = "kea_masterclasses";
                 
                 // run the query
                 
+                
                 $bResult = $query->execute();
-
+                
                 // If the query is successful the bResult will be true
-                $sjResponse = $bResult ? '{"status":"ok"}' : '{"status":"error"}' ;
-        
+                $sjResponse = $bResult ? '{"status":"ok, data saved to users table"}' : '{"status":"error, could not save data to users table"}' ;
+                
                 // Echo back to the client
                 echo $sjResponse; 
-        
-        
+                
+                
             } catch (Exception $e) {
-              
+                
                 echo "ERROR";
-        
+                
             }
+            
 
-?>
+
+            ?>
---
+++ databaseDump.sql
@@ -0,0 +1,153 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: kea_masterclasses
+-- ------------------------------------------------------
+-- Server version	5.5.41-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `attendance`
+--
+
+DROP TABLE IF EXISTS `attendance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attendance` (
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  KEY `fk_users_events_users1_idx` (`user_id`),
+  CONSTRAINT `fk_users_events_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendance`
+--
+
+LOCK TABLES `attendance` WRITE;
+/*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `notification` tinyint(4) NOT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `admin` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (5,'TESTNAME','TESTPASSWORD',0,'assets/img/userimage-5a16cbe54ea82.png',NULL,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_email`
+--
+
+DROP TABLE IF EXISTS `users_email`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_email` (
+  `email` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  KEY `fk_users_email_users` (`user_id`),
+  CONSTRAINT `fk_users_email_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_email`
+--
+
+LOCK TABLES `users_email` WRITE;
+/*!40000 ALTER TABLE `users_email` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_email` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_interests`
+--
+
+DROP TABLE IF EXISTS `users_interests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_interests` (
+  `users_id` int(11) NOT NULL,
+  `interests` varchar(45) NOT NULL,
+  KEY `fk_users_interests_users1_idx` (`users_id`),
+  CONSTRAINT `fk_users_interests_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_interests`
+--
+
+LOCK TABLES `users_interests` WRITE;
+/*!40000 ALTER TABLE `users_interests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_interests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_phone`
+--
+
+DROP TABLE IF EXISTS `users_phone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_phone` (
+  `users_id` int(11) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  KEY `fk_users_phone_users1` (`users_id`),
+  CONSTRAINT `fk_users_phone_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_phone`
+--
+
+LOCK TABLES `users_phone` WRITE;
+/*!40000 ALTER TABLE `users_phone` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_phone` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-23 14:27:23
