8ec34eea119a12d4ec10afc42d174b0450029299
Klauda <klaudiahuzova@gmail.com>
2017-11-26 20:28:27 +0100

    index.php and myEvents.php

--- app/index.php
+++ app/index.php
@@ -21,31 +21,101 @@
 	<div id="eventFilterSection">
 		<h2>events</h2>
 		<div id="filerButtons">
-			<aside>
-				<button id="filterPastEventBtn">past</button>
-				<button id="filterUpcommingEventBtn">upcomming</button>
-			</aside>
-			<section>
-				<button id="filterUiBtn">ui</button>
-				<button id="filterUxBtn">ux</button>
-				<button id="filterDevBtn">dev</button>				
+			<div id="eventsByTime">
+				<button id="filterPastEventBtn" class="filterByTime">past</button>
+				<button id="filterUpcommingEventBtn" class="filterByTime">upcomming</button>
+			</div>
+			<section id="eventsByType">
+				<div id="eventsByTypeWidth">
+				<button id="filterUiBtn" class="filterByType">ui</button>
+				<button id="filterUxBtn" class="filterByType">ux</button>
+				<button id="filterDevBtn" class="filterByType">dev</button>	
+				<div>			
 			</section>
-			<div>
+			<div id="searchEvent">
 				<input id="search" type="text" name="" placeholder="SEARCH">
 			</div>
 		</div>
 		<div id="eventBoxes">
+			<!-- <div>{{eventBox}}</div> -->
 			<div class="eventBox">
 				<div class="eventImg greenBorder"></div>
-				<p>Name:kadkkdj</p>
-				<p>Date:15.12.2017</p>
-				<p>Time:15:00</p>
-				<p>Description: Yes, all those months and years of planning, Valckes criticisms and Seth Balthermouth heartburn.</p>
-			</div>	
+				<div class="eventDetails">
+					<p>Name:kadkkdj</p>
+					<p>Date:15.12.2017</p>
+					<p>Time:15:00</p>
+					<p class="eventDescription">Description: Yes, all those months and years of planning, Valckes criticisms and Seth Balthermouth heartburn.</p>
+				</div>	
+			</div>		
+
+			<div class="eventBox">
+				<div class="eventImg greenBorder"></div>
+				<div class="eventDetails">
+					<p>Name:kadkkdj</p>
+					<p>Date:15.12.2017</p>
+					<p>Time:15:00</p>
+					<p class="eventDescription">Description: Yes, all those months and years of planning, Valckes criticisms and Seth Balthermouth heartburn.</p>
+				</div>	
+			</div>
+
+			<div class="eventBox">
+				<div class="eventImg greenBorder"></div>
+				<div class="eventDetails">
+					<p>Name:kadkkdj</p>
+					<p>Date:15.12.2017</p>
+					<p>Time:15:00</p>
+					<p class="eventDescription">Description: Yes, all those months and years of planning, Valckes criticisms and Seth Balthermouth heartburn.</p>
+				</div>	
+			</div>
+
+			<div class="eventBox">
+				<div class="eventImg greenBorder"></div>
+				<div class="eventDetails">
+					<p>Name:kadkkdj</p>
+					<p>Date:15.12.2017</p>
+					<p>Time:15:00</p>
+					<p class="eventDescription">Description: Yes, all those months and years of planning, Valckes criticisms and Seth Balthermouth heartburn.</p>
+				</div>	
+			</div>
+
 		</div>
 	</div>
 	<?php
 		include 'footer.html';
 	?>
+
+	<script>
+		var filterByType = document.getElementsByClassName("filterByType");
+		for ( i = 0; i < filterByType.length; i++){
+			filterByType[i].addEventListener("click", function(e){
+			  for ( j = 0; j < filterByType.length; j++){
+			  	if (e.target == filterByType[j]){
+			  		switch(j) {
+			  			case 0: filterByType[0].style.background = "#52B795"; break;
+			  			case 1: filterByType[1].style.background = "#EF4B47"; break;
+			  			case 2: filterByType[2].style.background = "#F9E131"; break;
+			  		}
+			  	} else {
+			  		filterByType[j].style.background = "#c1c1c1";
+			  		//filterByType[j].style.color = "#f7f7f7";
+			  	}
+			  }
+			});
+		}
+		
+		var filterByTime = document.getElementsByClassName("filterByTime");
+		for ( i = 0; i < filterByTime.length; i++){
+			filterByTime[i].addEventListener("click", function(e){
+			  for ( j = 0; j < filterByTime.length; j++){
+			  	if (e.target == filterByTime[j]){
+			  		filterByTime[j].style.background = "inherit";
+			  	} else {
+			  		filterByTime[j].style.background = "#c1c1c1";
+			  	}
+			  }
+			});
+		}
+
+	</script>
  </body>
 </html>
