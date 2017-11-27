8ab904dae055f614e163d989214c52679ceb564e
Patrick <brix.bjerg@gmail.com>
2017-11-27 12:18:31 +0100

    removed the "display" in the routes

--- api/node/server.js
+++ api/node/server.js
@@ -95,7 +95,7 @@ app.get('/delete-event', (req, res) => {
 })
 
 //DISPLAY ALL EVENTS
-app.get('/display-all-events', (req, res) => {
+app.get('/events', (req, res) => {
     event.getEvents((err, jStatus, ajEvents) => {
         if (err) {
             console.log(jStatus)
@@ -110,7 +110,7 @@ app.get('/display-all-events', (req, res) => {
 })
 
 //DISPLAY EVENT BY ID
-app.get('/display-event/:id', (req, res) => {
+app.get('/event/:id', (req, res) => {
     var iEventId = req.query.id
     event.displayEventById(iEventId, (err, jStatus, jEvent) => {
         if (err) {
--- api/php/get-all-events.php
+++ api/php/get-all-events.php
@@ -1,6 +1,6 @@
 <?php
 
-    $sEvents = file_get_contents("http://localhost:3333/display-all-events");
+    $sEvents = file_get_contents("http://localhost:3333/events");
     
     echo $sEvents;
 
