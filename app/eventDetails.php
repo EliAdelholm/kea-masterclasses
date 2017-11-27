e632d3ff8b81fc3d5f2efb163f3d12b430d06286
Klauda <klaudiahuzova@gmail.com>
2017-11-27 18:13:49 +0100

    fixing style and eventDescription.php

---
+++ app/eventDetails.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EVENT DETAILS</title>
+	<link rel="stylesheet" type="text/css" href="css/eventDetailsStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<?php
+		include 'nav.php';
+	?>
+	<div id="topImageSection">
+		<h1><span>DB EVENT</span></h1>
+	</div>
+	<div id="descriptionContainer">
+		<div id="containerHeadder">
+			<div id="tagStyle">UI</div>
+			<div><strong>DATE:</strong> 12.12.2017</div>
+			<div><strong>TIME:</strong> 15:00</div>
+			<div><strong>SPEAKER:</strong> ANNA HAPPY</div>
+		</div>
+		<div id="containerContent">
+			<form>
+				<div>
+	                <label><strong>DESCRIPTION</strong></label>
+	                <textarea name="Text1" cols="40" rows="4" disabled="disabled">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim</textarea>
+	            </div>
+	            <div class="form-group">
+	                <label><strong>REQUIREMENTS:</strong></label>
+	                <textarea name="Text1" cols="40" rows="1">Lorem ipsum dolor sit amet</textarea>
+	                <!-- <input type="text" value="Lorem ipsum dolor sit amet"> -->
+	            </div>
+				<div class="extraMarginBottom"><strong>ALREADY GOING:</strong> 20</div>
+				<div class="form-group">
+	                <label><strong>TO DO BEFORE EVENT:</strong></label>
+	                <textarea name="Text1" cols="40" rows="1">Sed diam nonummy nibh euismod tincidunt ut laoreet doloremagna aliquam erat volutpat</textarea>
+	                <!-- <input type="text" value="Sed diam nonummy nibh euismod tincidunt ut laoreet doloremagna aliquam erat volutpat"> -->
+	            </div>
+	            <div id="showToAdmin">
+<!-- 	            	<button>save changes</button>
+	            	<hr> -->
+	            </div>
+            </form>
+            <div>
+            	<button>participate in event</button>
+            	<button>cancel</button>
+            </div>
+		</div>
+
+	</div>
+
+	<?php
+		include 'footer.html';
+	?>
+	<script>
+		var amin = false;
+		if(amin){
+			var appendToForm = '<button>save changes</button><hr>';
+			showToAdmin.insertAdjacentHTML('beforeend', appendToForm);
+		}
+	</script>
+</body>
+</html>
