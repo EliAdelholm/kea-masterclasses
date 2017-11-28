5707b6ce8263b730d34e2f736ab1e4d60dd28205
Patrick <brix.bjerg@gmail.com>
2017-11-28 10:02:33 +0100

    Added login and input styling to global scss

--- app/login.php
+++ app/login.php
@@ -5,7 +5,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/global.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -23,7 +23,7 @@
             <div id="divLogin">
                 <form id="frmLogin">
                     <p> Login </p>
-                    <p>Username</p>
+                    <p> Username </p>
                     <input type="text" placeholder="User name" name="txtUserLoginName">
                     <p>Password</p>
                     <input type="text" placeholder="Password" name="txtUserLoginPassword">
