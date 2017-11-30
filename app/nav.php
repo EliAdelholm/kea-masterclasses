9885d322cbf36bda90d8bd311656297fdfc209e2
Eli <eliadelholm@gmail.com>
2017-11-30 09:17:02 +0100

    Nav stuff

--- app/nav.php
+++ app/nav.php
@@ -1,7 +1,19 @@
 <div id="positionAbsolut">
 	<nav class="main-container">
+	
+		<!-- ADMIN NAV ITEMS -->
+		<?php 
+			if($_SESSION['bAdmin']) {
+				echo '<a href="#" class="extraMargin">STATS</a>
+					  <a href="#" class="extraMargin">PENDING</a>';
+			}
+		?>
+
+		<!-- STATIC NAV ITEMS -->
 		<a href="index.php" class="extraMargin">EVENTS</a>
 		<a href="hold-masterclass.php" class="extraMargin">HOLD MASTERCLASS</a>
+
+		<!-- LOGIN OR PROFILE NAV ITEM -->
 		<?php
 			if (!isset($_SESSION['sUserId'])) {
 				echo '<a href="#" class="extraMargin">LOGIN</a>';
