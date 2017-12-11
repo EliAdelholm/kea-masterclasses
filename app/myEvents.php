55fb3395b27fe60f668b0ef754e9345ae8ecb3ef
Patrick <brix.bjerg@gmail.com>
2017-12-11 14:26:11 +0100

    Added image handling to the update event. Now when you delete stuff the images are also rem0oved

--- app/myEvents.php
+++ app/myEvents.php
@@ -43,9 +43,9 @@
 		<div id="eventBoxes">
 
 			<?php
-				// ini_set('display_errors', 1);
-				// ini_set('display_startup_errors', 1);
-				// error_reporting(E_ALL);	
+				ini_set('display_errors', 1);
+				ini_set('display_startup_errors', 1);
+				error_reporting(E_ALL);	
 				for($i = 0; $i < count($aEvents); $i++) {
 					$sEventId =  $aEvents[$i];
 					$sEvent = file_get_contents("http://localhost:3333/event/" . $sEventId);
