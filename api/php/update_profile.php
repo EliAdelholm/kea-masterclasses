0fe742c0416e53101a811da8bce0ef0e1b3522d6
magortako <takomagor@gmail.com>
2017-12-12 15:23:31 +0100

    so close yet so far :D ...getting closer to be done with profile :)) such a noob

--- api/php/update_profile.php
+++ api/php/update_profile.php
@@ -2,39 +2,43 @@
     session_start();
     include 'db.php';
 
-    $iUserId = $_SESSION['sUserId'];
+   $iUserId = $_POST['id'];
     $sEmail = $_POST['txtUserEmail'];
     $sName = $_POST['txtUserName'];
     $sPassword = $_POST['txtUserPassword'];
-    $sImg = 'ads';//$_POST['img'];
+    $sImg = $_POST['imgProfilePicture'];
     $sDesc = $_POST['txtUserDescription'];
-    $bNotif = $_POST['notification'] == "on" ? 1 : 0;
+    //$bNotif = $_POST['notification'] == "on" ? 1 : 0;
 
-    echo "userId " . $iUserId . "<br/>";
-    echo "email " . $sEmail . "<br/>";
-    echo "name " . $sName . "<br/>";
-    echo "name " . $sPassword . "<br/>";
-    echo "img: " . $sImg . "<br/>";
-    echo "desc " . $sDesc . "<br/>";
-    echo "notif: " . $bNotif . "<br/>";
 
-    $statement = "UPDATE users SET `description` = ':desc' WHERE `id` =:id;";
-    password = ':passwd', 
-    email = ':email', 
-    image =':img', 
-    `name`=':name', `notification` =':notif',
+    //$statement = "UPDATE users SET `description` = ':desc' WHERE `id` =:id;";
+    //password = ':passwd', 
+    //email = ':email', 
+    //image =':img', 
+    //`name`=':name',
+    //`notification` =':notif',
 
-    $query = $conn->prepare("UPDATE users SET name = :name, notification = :notif, description = :desc WHERE id =:id;"); 
+    $query = $conn->prepare("UPDATE users SET name = :name, password = :password, description = :description  WHERE id =:id;");
     // echo var_dump($statement);
 
     $query->bindParam( ':id' , $iUserId,  PDO::PARAM_INT );
-    // $query->bindParam( ':email' , $sEmail );
+    $query->bindParam( ':password' , $sPassword );
     $query->bindParam( ':name' , $sName );
-    // $query->bindParam( ':img' , $sImg );
-    $query->bindParam( ':desc' , $sDesc );
-    $query->bindParam( ':notif' , $bNotif,  PDO::PARAM_INT );
+   //$query->bindParam( ':img' , $sImg );
+    $query->bindParam( ':description' , $sDesc );
+    //$query->bindParam( ':notif' , $bNotif,  PDO::PARAM_INT );
 
-    $query->execute();        
+    $query->execute();
+
+
+   // $query = $conn->prepare("UPDATE users_emails SET email = :email  WHERE user_id =:user_id;")
+
+    //$query->bindParam( ':user_id' , $iUserId ,  PDO::PARAM_INT);
+    //$query->bindParam( ':email' , $sEmail );
+
+    //$query->execute();
+
+
 
     echo var_dump($query);
 ?>
