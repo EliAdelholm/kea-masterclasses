6091af6e50f57a6271b0383a8b9c56113062bbbf
magortako <takomagor@gmail.com>
2017-12-19 12:41:43 +0200

    styled UPLOAD IMAGE button on create-event page

--- app/index.php
+++ app/index.php
@@ -89,6 +89,30 @@
 	?>
 
 	<script>
+
+		// ******************* ADD IMAGE BEAUTIFY *********************
+		var inputs = document.querySelectorAll( '.inputfile' );
+		Array.prototype.forEach.call( inputs, function( input )
+		{
+		var label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener( 'change', function( e )
+		{
+			var fileName = '';
+			if( this.files && this.files.length > 0 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				label.querySelector( 'span' ).innerHTML = fileName;
+			else
+				label.innerHTML = labelVal;
+		});
+		});
+
+
 		// ******************* FILTER EVENTS BY TYPE *********************
 		var filterByType = document.getElementsByClassName("filterByType");
 		for ( i = 0; i < filterByType.length; i++){
